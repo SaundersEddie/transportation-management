c96f39f51c506032f42c3eb58ad2c5e1a5093036
Eddie Saunders <saunders.eddie@outlook.com>
2021-04-06 13:09:34 -0400

    Refactoring to ES6, and adding security

--- server/routes/load.js
+++ server/routes/load.js
@@ -1,10 +1,12 @@
 // Our Routes load
+// const express = require('express');
+const router = require('express').Router();
+const loadController = require ('../controllers/loadController')
 
-import express from 'express';
-import {allLoads, createLoad} from '../controllers/loadController.js';
-const router = express.Router();
+// import {allLoads, createLoad} from '../controllers/loadController.js';
+// const router = express.Router();
 
 router.get ('/', allLoads);
 router.post ('/', createLoad);
 
-export default router;
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import passport from 'passport';
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require ('body-parser');
+const mongoose = require ('mongoose');
+const morgan = require ('morgan');
+const passPort = require ('passport');
+require ('dotenv').config();
 
-import ourLoads from './routes/load.js';
-import ourUsers from './routes/user.js';
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 const ATLAS_URI = process.env.MONGO_DB;
-const app = express();
+const server = express();
+
+const ourLoads = require ('./routes/load.js');
+const ourUsers = require ('./routes/user.js');
+
 
 if (process.env.NODE_ENV !== "production") {
     console.log("loading dev environment.");
@@ -21,10 +22,10 @@ mongoose.connect(ATLAS_URI,
                 {useNewUrlParser: true, 
                 useCreateIndex: true,
                 useUnifiedTopology: true })
-        .then(() => app.listen(PORT, () => console.log(`API Server listening on ${PORT}`)))
-        .then(() => app.use(bodyParser.json({ limit: "30mb", extended: true })))
-        .then(() => app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })))
-        .then(() => app.use('/u', ourUsers))
-        .then(() => app.use('/l', ourLoads))
+        .then(() => server.listen(PORT, () => console.log(`API Server listening on ${PORT}`)))
+        .then(() => server.use(bodyParser.json({ limit: "30mb", extended: true })))
+        .then(() => server.use(bodyParser.urlencoded({ limit: "30mb", extended: true })))
+        .then(() => server.use('/u', ourUsers))
+        .then(() => server.use('/l', ourLoads))
         .catch((error) => console.log("An Error Occurred: ", error)
       );
