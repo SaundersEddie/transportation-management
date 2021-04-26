3110125ec863d76f6ca7d3308f39773742466349
Eddie Saunders <saunders.eddie@outlook.com>
2021-04-26 15:20:35 -0400

    Formatting on signup/login form

--- client/src/pages/splash.js
+++ client/src/pages/splash.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { Button, Row, Modal } from 'react-bootstrap';
 import warningLogo from '../assets/images/warning_sign.png';
+import Login from './Login/Login';
 
 // Click this button will actually take you direct to the
 // mini-TMS page, authentication will be added later on
@@ -14,13 +15,16 @@ export default function Splash() {
             <img className = "center-image" src={warningLogo} alt="warning sign"/>
             <h1 align="center">Authourized Users Only</h1>
             <Row className="border-top justify-content-center p-4">
-                <Button align="center" size="lg" variant="outline-primary" onClick={handleShow}>Login</Button>
+                <Button align="center" size="lg" variant="outline-primary" onClick={handleShow}>Register or Sign In</Button>
             </Row>
+
             <Modal size="lg" show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Please enter your credentials</Modal.Title>
+                    <Modal.Title>Signup</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Our login form will go here</Modal.Body>
+                <Modal.Body>
+                    <Login />
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>Close</Button>
                 </Modal.Footer>
